fec5155cf596ae84a0b41304b473d2f8f231f56a
Nadin Novoselova <nadja.novoselova@gmail.com>
2025-03-10 22:46:57 +0300

    feat: add mergeObjects

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,8 +17,8 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-function shallowCopy(obj) {
-  return { ...obj };
+function shallowCopy(Object) {
+  return { ...Object };
 }
 
 /**
@@ -32,8 +32,20 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const result = {};
+
+  objects.forEach((object) => {
+    Object.entries(object).forEach(([key, value]) => {
+      if (result[key]) {
+        result[key] += value;
+      } else {
+        result[key] = value;
+      }
+    });
+  });
+
+  return result;
 }
 
 /**
